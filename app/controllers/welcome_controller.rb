1e376e7e96ab3f6b04f12b995002f4d802e67b3b
arangutan <santiago.varon@gmail.com>
2014-07-01 18:35:55 -0500

    home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   	#User.create(email: 'superadmin', password:'1234rewq', cargo: 'developer', nombre: 'santiago', apellido: 'arango', correo_electronico: 'santiago.varon@gmail.com', telefono: '3008911930', empresa_id: '1') 
-
+  	if user_signed_in?
+  		redirect_to stakeholders_path
+  	end
   end
 end
