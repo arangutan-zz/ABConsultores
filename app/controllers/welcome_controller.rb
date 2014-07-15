3edb71e95395a2b2f9447b8e8b65cd31a455a6ac
arangutan <santiago.varon@gmail.com>
2014-07-15 18:55:03 -0500

    home responsive

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
-  def index
-  	#User.create(email: 'superadmin', password:'1234rewq', cargo: 'developer', nombre: 'santiago', apellido: 'arango', correo_electronico: 'santiago.varon@gmail.com', telefono: '3008911930', empresa_id: '1') 
-  	if user_signed_in?
-  		redirect_to stakeholders_path
-  	end
-  end
+	layout "welcome"
+	
+	def index
+	#User.create(email: 'superadmin', password:'1234rewq', cargo: 'developer', nombre: 'santiago', apellido: 'arango', correo_electronico: 'santiago.varon@gmail.com', telefono: '3008911930', empresa_id: '1') 
+	if user_signed_in?
+	 	redirect_to stakeholders_path
+		end
+	end
 end
